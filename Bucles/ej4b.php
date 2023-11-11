389c7a295698c4086f22114376c33b6ccb378917
MabelFlores <126671144+MabelFlores@users.noreply.github.com>
2023-11-11 17:53:52 +0100

    Ej4b.php

---
+++ Bucles/ej4b.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD><Title>Tabla Multiplicar</Title></HEAD>
+<BODY>
+<?php
+
+$num="8";
+
+for ($i = 1; $i <= 10; $i++) {
+    $resultado = $num * $i;
+    echo "$num x $i = $resultado<br>";
+}
+?>
+</BODY>
+</HTML>
