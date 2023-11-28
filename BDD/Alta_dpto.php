744d945ee11f41e113a93b9c0429adf1a6917b72
MabelFlores <126671144+MabelFlores@users.noreply.github.com>
2023-11-28 11:02:19 +0100

    Create Alta_dpto.php

---
+++ BDD/Alta_dpto.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>  
+<style>
+    body {
+      text-align: center;
+    }
+  </style>
+<body>
+
+<h2>ALTA DEPARTAMENTO</h2>
+
+<form name='alta_deptoform' action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='POST'>
+COD_DPTO: <input type="text" name="cod_dpto" size=15><br>
+<br>
+NOMBRE: <input type="text" name="nombre_dpto" size=15><br>
+<br>
+<input type="submit" value="Crear Dpto" >
+</form>
+<?php
+/*Inserción en tabla Prepared Statement- mysql PDO*/
+
+$servername = "localhost";
+$username = "root";
+$password = "rootroot";
+$dbname = "empleadosnn";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // prepare sql and bind parameters
+    $stmt = $conn->prepare("INSERT INTO departamento (cod_dpto,nombre_dpto) VALUES (:cod_dpto,:nombre_dpto)");
+    $stmt->bindParam(':cod_dpto', $cod_dpto);
+    $stmt->bindParam(':nombre_dpto', $nombre);
+  
+    // insert a row
+    $cod_dpto = $_POST['cod_dpto'];
+	$nombre = $_POST['nombre_dpto'];
+    $stmt->execute();
+    echo "New records created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+?>
+
+</body>
+</html>
